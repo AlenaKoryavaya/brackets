b0741d61e95e508fe312545e397e4a2d2bcbce58
Alena Koryavaya <elena.koryavaya@gmail.com>
2022-08-08 12:27:35 +0300

    added changes to the index.js file (the task is not complete)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openBrackets = [];
+  let bracketsPair = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let prop = bracketsConfig[i][1];
+    let value = bracketsConfig[i][0];
+    openBrackets.push(bracketsConfig[i][0]);
+    bracketsPair[prop] = value;
+  }
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentValue = str[i];
+
+    if (openBrackets.includes(currentValue)) {
+      stack.push(currentValue);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElem = stack[stack.length - 1];
+
+      if (bracketsPair[currentValue] === topElem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
